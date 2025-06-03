30169d1ed4b89a0759a80ecbc4c8ee6d6f22ad7e
Maurice <mau.topf@gmail.com>
2025-06-03 21:01:44 +0200

    from Java 2 Kotlin

--- src/main/java/Bot.kt
+++ src/main/java/Bot.kt
@@ -1,5 +1,13 @@
-public class Bot {
-    public static void main(String[] args) {
-        // here comes the code :^)
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.OnlineStatus
+import net.dv8tion.jda.api.entities.Activity
+
+object Bot {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val jda: JDABuilder = JDABuilder.createLight(args[0])
+        jda.setStatus(OnlineStatus.IDLE)
+        jda.setActivity(Activity.customStatus("( ͡° ͜ʖ ͡°)"))
+        jda.build().awaitReady()
     }
 }
